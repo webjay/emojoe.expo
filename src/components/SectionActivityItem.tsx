f1f8f52d48a964d4941ab94e07c440908bbe22b8
Jacob Friis Saxberg <jacob@webcom.dk>
2023-01-20 22:50:53 +0100

    pure

--- src/components/SectionActivityItem.tsx
+++ src/components/SectionActivityItem.tsx
@@ -1,11 +1,9 @@
-import React, { useMemo, useState, useEffect } from 'react';
+import React, { useMemo } from 'react';
 import { StyleSheet, View } from 'react-native';
 import type { StyleProp, ViewStyle } from 'react-native';
-import { Chip, ActivityIndicator } from 'react-native-paper';
 import type { ActivityItem } from '../types/common';
 import { dayProgressFlex } from '../lib/date';
-import { profileGetById } from '../lib/api';
-import type { Profile } from '../types/api';
+import NameChip from './NameChip';
 import Emoji from './Emoji';
 
 type Props = {
@@ -29,26 +27,14 @@ const styles = StyleSheet.create({
   },
 });
 
-function firstName(name: Profile['name']) {
-  if (!name) return '🃟';
-  return name.substring(0, name.indexOf(' '));
-}
-
 export default function SectionActivityItem({ item: { createdAt, emoji, profileId } }: Props) {
-  const [profile, setProfile] = useState<Profile>();
-  const avatar = useMemo(() => (profile ? null : <ActivityIndicator />), [profile]);
   const progressSpaceStyle = useMemo<StyleProp<ViewStyle>>(() => ({
     flex: dayProgressFlex(createdAt),
   }), [createdAt]);
-  useEffect(() => {
-    profileGetById(profileId).then(setProfile);
-  }, [profileId]);
   return (
     <View style={styles.container}>
       <View style={styles.chipView}>
-        <Chip avatar={avatar} mode="outlined" compact>
-          {firstName(profile?.name)}
-        </Chip>
+        <NameChip profileId={profileId} />
         <View style={styles.chipSpace} />
       </View>
       <View style={progressSpaceStyle} />
