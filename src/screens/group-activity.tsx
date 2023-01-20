b3bca18c366b77499404b85ff675e2c63522a9ff
Jacob Friis Saxberg <jacob@webcom.dk>
2023-01-20 17:24:00 +0100

    section header

--- src/screens/group-activity.tsx
+++ src/screens/group-activity.tsx
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, SafeAreaView, SectionList } from 'react-native';
 import type { ScreenPropsStack } from '../types/navigation';
 import useGroup from '../hooks/useGroup';
 import type { Activity } from '../types/api';
 import { groupGetActivities } from '../lib/api';
-import renderItem from '../components/section-activity';
+import SectionActivityHeader from '../components/SectionActivityHeader';
+import { renderItem } from '../components/SectionActivityItem';
+import type { ActivitySection, ActivitySectionMap } from '../types/common';
+import { toDateString, dayTitle } from '../lib/date';
 
 type Props = ScreenPropsStack<'GroupActivity'>;
+
+type SectionsMap = Map<string, ActivitySectionMap>;
 
 const styles = StyleSheet.create({
   container: {
@@ -15,28 +20,60 @@ const styles = StyleSheet.create({
   },
 });
 
+function sectionsMapToArray(map: SectionsMap) {
+  return Array.from(map, ([, item]) => item);
+}
+
+function makeSections(activities: Activity[]) {
+  const sections: SectionsMap = new Map();
+  activities.forEach(({ id, createdAt, emoji }) => {
+    const createdAtDateString = toDateString(createdAt);
+    if (!sections.has(createdAtDateString)) {
+      sections.set(createdAtDateString, {
+        title: dayTitle(new Date(createdAt)),
+        data: [],
+      });
+    }
+    const section = sections.get(createdAtDateString);
+    section?.data.push({
+      id,
+      createdAt,
+      emoji,
+    });
+  });
+  return sections;
+}
+
+const renderSectionHeader = ({ section }: { section: ActivitySection }) => (
+  <SectionActivityHeader section={section} />
+);
+
 export default function GroupActivityScreen({ route: { params: { groupId } } }: Props) {
   const { setOptions } = useNavigation();
   const { group } = useGroup(groupId);
-  const [activities, setActivities] = useState<Activity[]>([]);
-  useEffect(() => {
-    groupGetActivities(groupId).then(setActivities);
+  const [sections, setSections] = useState<ActivitySection[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const loadData = useCallback(async () => {
+    setRefreshing(true);
+    await groupGetActivities(groupId).then(makeSections).then(sectionsMapToArray).then(setSections);
+    setRefreshing(false);
   }, [groupId]);
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
   useEffect(() => {
     if (!group) return;
     setOptions({ title: group.name });
   }, [group, setOptions]);
-  const sections = [
-    {
-      title: '',
-      data: activities,
-    },
-  ];
   return (
     <SafeAreaView style={styles.container}>
       <SectionList
         sections={sections}
+        renderSectionHeader={renderSectionHeader}
         renderItem={renderItem}
+        onRefresh={loadData}
+        refreshing={refreshing}
+        stickySectionHeadersEnabled
       />
     </SafeAreaView>
   );
