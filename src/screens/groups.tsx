3b45a447b18e7c901d3f58c792e3ec5e4022bbc7
Jacob Friis Saxberg <jacob@webcom.dk>
2023-02-04 12:03:51 +0100

    scroll hack

--- src/screens/groups.tsx
+++ src/screens/groups.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import { useIsFocused } from '@react-navigation/native';
+import React, { useState, useCallback } from 'react';
+import { StyleSheet, InteractionManager } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import useGroupMemberships from '../hooks/useGroupMemberships';
-import GroupCard from '../components/GroupCard';
+import Loading from '../components/Loading';
+import Empty from '../components/Empty';
 import ScrollViewRefresh from '../components/ScrollViewRefresh';
+import GroupCard from '../components/GroupCard';
 import SafeAreaBottom from '../components/SafeAreaBottom';
 
 const styles = StyleSheet.create({
@@ -13,13 +15,21 @@ const styles = StyleSheet.create({
 });
 
 export default function GroupsScreen() {
-  const isFocused = useIsFocused();
+  const [waiting, setWaiting] = useState(true);
   const { loading, groups, loadData } = useGroupMemberships();
-  useEffect(() => {
-    if (isFocused) loadData(false);
-  }, [isFocused, loadData]);
+  useFocusEffect(useCallback(() => {
+    loadData(false);
+    // hack to make sure scrollbars are set for full width
+    InteractionManager.runAfterInteractions(() => {
+      setTimeout(() => setWaiting(false), 500);
+    });
+  }, [loadData]));
+  if (waiting) return <Loading />;
   return (
     <ScrollViewRefresh loading={loading} refetch={loadData} style={styles.container}>
+      {groups.length === 0 && !loading && (
+        <Empty />
+      )}
       {groups.map((group) => (
         <GroupCard key={group.groupId} group={group} />
       ))}
