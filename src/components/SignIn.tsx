a1a411961de246bb8513bf4a5f62f389984ebf8f
Jacob Friis Saxberg <jacob@webcom.dk>
2023-02-19 17:15:24 +0100

    cleanup

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,11 +1,7 @@
 import React, { useCallback } from 'react';
 import { StyleSheet, View, Pressable } from 'react-native';
 import { useTheme, Text, Button } from 'react-native-paper';
-// import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 import { Auth } from 'aws-amplify';
-// import { MaterialCommunityIcons } from '@expo/vector-icons';
-// import AppleSigninIcon from './Icons/AppleSignin';
-// import GoogleSigninIcon from './Icons/GoogleSignin';
 
 const styles = StyleSheet.create({
   container: {
@@ -32,16 +28,6 @@ export default function Signin() {
   const signIn = useCallback(() => {
     Auth.federatedSignIn();
   }, []);
-  // const signInWithApple = useCallback(() => {
-  //   Auth.federatedSignIn({
-  //     provider: CognitoHostedUIIdentityProvider.Apple,
-  //   });
-  // }, []);
-  // const signInWithGoogle = useCallback(() => {
-  //   Auth.federatedSignIn({
-  //     provider: CognitoHostedUIIdentityProvider.Google,
-  //   });
-  // }, []);
   return (
     <View style={[styles.container, { backgroundColor }]}>
       <View style={styles.content}>
@@ -52,12 +38,6 @@ export default function Signin() {
           <Pressable onPress={signIn}>
             <Button mode="contained">Sign in</Button>
           </Pressable>
-          {/* <Pressable onPress={signInWithApple}>
-            <MaterialCommunityIcons name="apple" size={48} color={color} />
-          </Pressable>
-          <Pressable onPress={signInWithGoogle}>
-            <MaterialCommunityIcons name="google" size={48} color={color} />
-          </Pressable> */}
         </View>
       </View>
     </View>
