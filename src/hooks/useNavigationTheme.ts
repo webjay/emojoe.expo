555f2d63f97e2655742e07487776a8c79e156ad3
Jacob Friis Saxberg <jacob@webcom.dk>
2023-04-09 15:01:56 +0200

    optimize

--- src/hooks/useNavigationTheme.ts
+++ src/hooks/useNavigationTheme.ts
@@ -1,16 +1,17 @@
 import { useMemo } from 'react';
 import { useColorScheme } from 'react-native';
-import { DefaultTheme } from '@react-navigation/native';
 import { adaptNavigationTheme } from 'react-native-paper';
+import {
+  DefaultTheme as reactNavigationLight,
+  DarkTheme as reactNavigationDark,
+} from '@react-navigation/native';
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight,
+  reactNavigationDark,
+});
 
 export default function useNavigationTheme() {
   const colorScheme = useColorScheme();
-  return useMemo(() => {
-    if (colorScheme === 'dark') {
-      const { DarkTheme } = adaptNavigationTheme({ reactNavigationDark: DefaultTheme });
-      return DarkTheme;
-    }
-    const { LightTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme });
-    return LightTheme;
-  }, [colorScheme]);
+  return useMemo(() => colorScheme === 'dark' ? DarkTheme : LightTheme, [colorScheme]);
 }
