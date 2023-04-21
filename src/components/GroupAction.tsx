ef2a5beb8daddc33da11e853bf6ec81f7923456e
Jacob Friis Saxberg <jacob@webcom.dk>
2023-04-21 21:57:50 +0200

    swipe better

--- src/components/GroupAction.tsx
+++ src/components/GroupAction.tsx
@@ -1,13 +1,11 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { StyleSheet } from 'react-native';
-import { useRouter } from 'expo-router';
 import { Card } from 'react-native-paper';
 import type { GroupMembership } from '../types/api';
-import Swipe from './Swipe';
+import Swipe from './Swipe/Swipe';
 
 type Props = {
   group: GroupMembership;
-  isSwiping: (swiping: boolean) => void;
 };
 
 const styles = StyleSheet.create({
@@ -20,29 +18,11 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function GroupAction({
-  group: { groupId, emoji },
-  isSwiping,
-}: Props) {
-  const { push: routerPush } = useRouter();
-  const handleDone = useCallback(() => {
-    routerPush({
-      pathname: `/group/${groupId}/done`,
-      params: {
-        // groupId,
-        emoji: emoji ?? '🃟',
-      },
-    });
-  }, [groupId, emoji, routerPush]);
+export default function GroupAction({ group: { groupId, emoji } }: Props) {
   return (
     <Card style={styles.container}>
       <Card.Content style={styles.content}>
-        <Swipe
-          emoji={emoji || '🃟'}
-          groupId={groupId}
-          isSwiping={isSwiping}
-          handleDone={handleDone}
-        />
+        <Swipe emoji={emoji || '🃟'} groupId={groupId} />
       </Card.Content>
     </Card>
   );
