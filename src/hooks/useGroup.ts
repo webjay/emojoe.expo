5c546e82688260a20146fe2ab3ef172aaade2aab
Jacob Friis Saxberg <jacob@webcom.dk>
2023-05-02 21:17:40 +0200

    use public name

--- src/hooks/useGroup.ts
+++ src/hooks/useGroup.ts
@@ -1,12 +1,19 @@
 import { useState, useEffect } from 'react';
-import type { Group } from '../types/api';
-import { groupGetPublic } from '../lib/api';
+import type { Group } from '@src/types/api';
+import { groupGetPublic } from '@src/lib/api';
+import groups from '@src/lib/groups';
 
-export default function useGroup(id?: Group['id']) {
+export default function useGroup(groupId?: Group['id']) {
   const [group, setGroup] = useState<Group>();
   useEffect(() => {
-    if (!id) return;
-    groupGetPublic(id).then(setGroup);
-  }, [id]);
+    if (!groupId) return;
+    groupGetPublic(groupId).then((groupFromDb) => {
+      const isPublic = groupId in groups;
+      setGroup({
+        ...groupFromDb,
+        name: isPublic ? groups[groupId].name : groupFromDb.name,
+      });
+    });
+  }, [groupId]);
   return { group };
 }
--- src/hooks/useGroupMemberships.ts
+++ src/hooks/useGroupMemberships.ts
@@ -1,6 +1,21 @@
 import { useState, useCallback } from 'react';
-import type { GroupMembership } from '../types/api';
-import { groupsByProfile } from '../lib/api';
+import type { GroupMembership } from '@src/types/api';
+import { groupsByProfile } from '@src/lib/api';
+import groupsPublic from '@src/lib/groups';
+
+function groupMap(groupMembership: GroupMembership) {
+  const isPublic = groupMembership.groupId in groupsPublic;
+  if (isPublic) {
+    return {
+      ...groupMembership,
+      group: {
+        ...groupMembership.group,
+        name: groupsPublic[groupMembership.groupId].name,
+      },
+    };
+  }
+  return groupMembership;
+}
 
 export default function useGroupMemberships() {
   const [loading, setLoading] = useState(false);
@@ -8,7 +23,7 @@ export default function useGroupMemberships() {
   const loadData = useCallback(async (signal = true) => {
     if (signal) setLoading(true);
     const data = await groupsByProfile();
-    setGroups(data);
+    setGroups(data.map(groupMap));
     if (signal) setLoading(false);
   }, []);
   return { loading, groups, loadData };
