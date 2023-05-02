de583fe2d1014e9612b3e87e02f2b68d3a99b12f
Jacob Friis Saxberg <jacob@webcom.dk>
2023-05-02 18:06:11 +0200

    /join/🏃

---
+++ app/(stack)/join/[emoji]/index.ts
@@ -0,0 +1,26 @@
+import { useSearchParams } from 'expo-router';
+import groups from '@src/lib/groups.json';
+
+const groupMap = new Map(
+  Object.entries(groups).map(([key, { emoji }]) => [emoji, key]),
+);
+
+type SearchParams = {
+  emoji: string | string[];
+};
+
+function redirect(path: string) {
+  window.location.replace(path);
+}
+
+export default function EmojiJoin() {
+  const { emoji } = useSearchParams<SearchParams>();
+  if (typeof emoji === 'string') {
+    const groupId = groupMap.get(emoji);
+    if (groupId) {
+      redirect(`/group/${groupId}/join`);
+      return;
+    }
+  }
+  redirect('/');
+}
