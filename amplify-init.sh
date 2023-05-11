0f180f9dbd22852731de61932c1434e2e1d33398
Jacob Friis Saxberg <jacob@webcom.dk>
2023-05-11 19:26:13 +0200

    Amplify init

---
+++ amplify-init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+# IFS='|'
+
+REACTCONFIG="{\
+\"SourceDir\":\"src\",\
+\"DistributionDir\":\"dist\",\
+\"BuildCommand\":\"npm run-script build\",\
+\"StartCommand\":\"npm run-script start\"\
+}"
+
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":false,\
+\"accessKeyId\":\"AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"AWS_SECRET_ACCESS_KEY\",\
+\"region\":\"eu-central-1\"\
+}"
+
+AMPLIFY="{\
+\"projectName\":\"emojoeexpo\",\
+\"appId\":\"d249ll2h4fem2z\",\
+\"envName\":\"dev\",\
+\"defaultEditor\":\"code\"\
+}"
+
+FRONTEND="{\
+\"frontend\":\"javascript\",\
+\"framework\":\"react-native\",\
+\"config\":$REACTCONFIG}"
+
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG}"
+
+npm run amplify pull -- --providers $PROVIDERS --amplify $AMPLIFY --frontend $FRONTEND --yes
