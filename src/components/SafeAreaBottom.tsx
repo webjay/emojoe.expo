a7b9b785f1916e5796bc049708f435ea9c18d389
Jacob Friis Saxberg <jacob@webcom.dk>
2023-05-14 13:40:06 +0200

    fix for Android

--- src/components/SafeAreaBottom.tsx
+++ src/components/SafeAreaBottom.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Platform, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+const styles = StyleSheet.create({
+  view: {
+    height: 10,
+  },
+});
+
 export default function SafeAreaBottom() {
-  return <SafeAreaView edges={['bottom']} />;
+  const style = Platform.OS === 'android' ? styles.view : {};
+  return <SafeAreaView edges={['bottom']} style={style} />;
 }
