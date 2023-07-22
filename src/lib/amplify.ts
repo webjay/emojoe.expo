de90908c2c2d080bf8f15d34e0970eac4bceb459
Jacob Friis Saxberg <jacob@webcom.dk>
2023-07-22 22:12:13 +0200

    / works for all now?

--- src/lib/amplify.ts
+++ src/lib/amplify.ts
@@ -1,12 +1,9 @@
 import { Amplify } from '@aws-amplify/core';
-import { isDevice } from 'expo-device';
-import { Platform } from 'react-native';
 import { createURL } from 'expo-linking';
 import awsConfig from '../aws-exports';
 import urlOpener from './urlOpener';
 
-const redirectUrl =
-  isDevice && Platform.OS !== 'web' ? createURL('') : createURL('/');
+const redirectUrl = createURL('/');
 
 const updatedAwsConfig = {
   ...awsConfig,
