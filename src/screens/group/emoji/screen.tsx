8233bdc346db7fdbb3a90d3e7ab924af386d4ea8
Jacob Friis Saxberg <jacob@webcom.dk>
2023-07-23 14:12:35 +0200

    capture press when keyboard is active

--- src/screens/group/emoji/screen.tsx
+++ src/screens/group/emoji/screen.tsx
@@ -63,6 +63,7 @@ export default function GroupEmoji() {
         renderItem={renderItemCallback}
         stickySectionHeadersEnabled
         showsVerticalScrollIndicator={false}
+        keyboardShouldPersistTaps="handled"
       />
     </Container>
   );
