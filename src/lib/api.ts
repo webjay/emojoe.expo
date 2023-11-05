5a26df404bbdf5501ffa888adaecdc46a0e8ca9e
Jacob Friis Saxberg <jacob@webcom.dk>
2023-11-05 11:29:44 +0100

    use GraphQL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-import { API, graphqlOperation } from '@aws-amplify/api';
+import { GraphQLAPI as API, graphqlOperation } from '@aws-amplify/api-graphql';
 import type { GraphQLResult } from '@aws-amplify/api-graphql';
 import getCognitoUserSub from '@src/lib/cognito';
 import {
