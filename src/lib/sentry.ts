53bf9f5faf0c7fb80e3dde5d43dc4a72bf6745f7
Jacob Friis Saxberg <jacob@webcom.dk>
2023-11-07 09:13:18 +0100

    capture in dev

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -3,8 +3,7 @@ import * as Sentry from 'sentry-expo';
 
 Sentry.init({
   dsn: 'https://7dc6ac81d3574ba88c10141830bd4307@o373802.ingest.sentry.io/4504628449116160',
-  // enableInExpoDevelopment: true,
-  // debug: true,
+  enableInExpoDevelopment: true,
 });
 
 export function wrap<P extends Record<string, any>>(
